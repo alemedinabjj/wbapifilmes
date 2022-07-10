27aed245f069899924fd725ee5cbd4b7dde481e0
Alexandre de Souza Medina <alemedina@MacBook-de-Alexandre.local>
2022-07-10 12:52:08 -0300

    add

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom'
 
 
-const searchURL = import.meta.env.VITE_SEARCH
+const searchURL = 'https://api.themoviedb.org/3/search/movie/'
 const apiKey = import.meta.env.VITE_API_KEY
 
 
