e37513dac4982b4dfd474a9632f36c58e02f35a5
Alexandre Medina <alesurf13@gmail.com>
2022-10-09 11:12:30 -0300

    feat pagination genres

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+export default function BasicPagination({ setPage, page }) {
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <div className="p-5">
+      <Stack spacing={2}>
+        <Pagination count={100} onChange={(event) => handleChange(event, event.target.textContent)} color="primary" />
+      </Stack>
+    </div>
+  );
+}
--- src/pages/Genres.jsx
+++ src/pages/Genres.jsx
@@ -1,27 +1,30 @@
 import { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { CardMovie } from "../components/Card";
+import BasicPagination from "../components/Pagination";
 
 const APIkey = import.meta.env.VITE_API_KEY;
 
 export const Genres = () => {
+  const [page, setPage] = useState(4);
   const [movies, setMovies] = useState([]);
   const { id } = useParams();
 
   const getMovies = useCallback(async () => {
-    const url = `https://api.themoviedb.org/3/discover/movie?${APIkey}&language
-        =pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}`;
+    const url = `
+    https://api.themoviedb.org/3/discover/movie?${APIkey}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${id}
+    `;
     const res = await fetch(url);
     const data = await res.json();
 
     setMovies(data.results);
-  }, [id]);
+  }, [id, page]);
 
-  console.log(movies);
+  console.log(page);
 
   useEffect(() => {
     getMovies();
-  }, [getMovies]);
+  }, [page, getMovies]);
 
   return (
     <div className="flex flex-col justify-center items-center dark:bg-slate-800 min-h-screen">
@@ -30,6 +33,7 @@ export const Genres = () => {
           return <CardMovie movie={movie} />;
         })}
       </section>
+      <BasicPagination setPage={setPage} />
     </div>
   );
 };
